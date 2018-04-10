fa1f9332161d48f832f15ac507d2d9e3601346b0
Adi Thakker <athakker@medsyshcs.com>
2018-04-10 16:11:16 -0400

    Added DependencyFactory.

---
+++ WebApi.Monitoring.Domain/DependencyFactory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace WebApi.Monitoring.Domain
+{
+    public static class DependencyFactory
+    {
+        private static ConcurrentDictionary<Type, Func<object>> _lookup = new ConcurrentDictionary<Type, Func<object>>();
+
+        public static void Set<TType>(Type key, Func<TType> create) where TType : class, new()
+        {
+            if (key == null)
+                throw new ArgumentNullException(nameof(key));
+
+            if (create == null)
+                throw new ArgumentNullException(nameof(create));
+
+
+            _lookup.AddOrUpdate(key, create, (index, createFunc) => createFunc);
+        }
+
+        public static TResult Get<TResult>() where TResult : Type
+        {
+            var func = _lookup.GetValueOrDefault(typeof(TResult));
+
+            if (func == null)
+                throw new InvalidOperationException($"No dependency registered for {typeof(TResult)}");
+
+            return (TResult)func();
+        }
+    }
+}
